d542ee511ebb0d9bbe9f7b81448d56a0c68237e3
PabloUrra2233 <purra2025@alu.uct.cl>
2026-05-29 16:33:59 -0400

    Implement Observer and Factory patterns

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common"
 import { PostsController } from "@/posts/posts.controller"
+import {
+    DomainEventLogger,
+    NotificationObserver,
+    PostEventPublisher,
+    RelevanceRecomputeObserver,
+} from "@/posts/events/post-event.publisher"
+import { PostEntityFactory } from "@/posts/factories/post-entity.factory"
 import { PostsService } from "@/posts/posts.service"
 
 @Module({
     controllers: [PostsController],
-    providers: [PostsService],
+    providers: [
+        PostsService,
+        PostEntityFactory,
+        PostEventPublisher,
+        DomainEventLogger,
+        NotificationObserver,
+        RelevanceRecomputeObserver,
+    ],
 })
 export class PostsModule {}
